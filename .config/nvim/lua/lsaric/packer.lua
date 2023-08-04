924a1906b1fa472aea59b6ed0b75be403c160afc
Luka Saric <rpg.ari7@gmail.com>
2023-08-04 11:33:35 +0200

    Remove lsp saga & use native lsp utils

--- .config/nvim/lua/lsaric/packer.lua
+++ .config/nvim/lua/lsaric/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -19,85 +19,83 @@ vim.cmd([[
 ]])
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	use("nvim-lua/plenary.nvim")
-	use("nvim-lua/popup.nvim")
+  use("wbthomason/packer.nvim")
+  use("nvim-lua/plenary.nvim")
+  use("nvim-lua/popup.nvim")
 
-	-- Tokyonight theme
-	use("folke/tokyonight.nvim")
+  -- Tokyonight theme
+  use("folke/tokyonight.nvim")
 
-	-- tmux & split window navigation
-	use("christoomey/vim-tmux-navigator")
+  -- tmux & split window navigation
+  use("christoomey/vim-tmux-navigator")
 
-	-- maximizes and restores current window
-	use("szw/vim-maximizer")
+  -- maximizes and restores current window
+  use("szw/vim-maximizer")
 
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = { "nvim-tree/nvim-web-devicons" },
-	})
-	-- fuzzy finding w/ telescope
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+  use({
+    "nvim-tree/nvim-tree.lua",
+    requires = { "nvim-tree/nvim-web-devicons" },
+  })
+  -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-	-- add, delete, change surroundings (it's awesome)
-	use("tpope/vim-surround")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use("tpope/vim-commentary")
-	use("mhinz/vim-signify")
-	use("mattn/emmet-vim")
-	use("ntpeters/vim-better-whitespace")
+  -- add, delete, change surroundings (it's awesome)
+  use("tpope/vim-surround")
+  use("JoosepAlviste/nvim-ts-context-commentstring")
+  use("tpope/vim-commentary")
+  use("mhinz/vim-signify")
+  use("mattn/emmet-vim")
+  use("ntpeters/vim-better-whitespace")
 
-	-- Status line
-	use("nvim-lualine/lualine.nvim")
+  -- Status line
+  use("nvim-lualine/lualine.nvim")
 
-	-- autocompletion
-	use("hrsh7th/nvim-cmp") -- completion plugin
-	use("hrsh7th/cmp-buffer") -- source for text in buffer
-	use("hrsh7th/cmp-path") -- source for file system paths
-	use("onsails/lspkind.nvim") -- vscode like icons for completion
+  -- autocompletion
+  use("hrsh7th/nvim-cmp") -- completion plugin
+  use("hrsh7th/cmp-buffer") -- source for text in buffer
+  use("hrsh7th/cmp-path") -- source for file system paths
+  use("onsails/lspkind.nvim") -- vscode like icons for completion
 
-	-- snippets
-	use("L3MON4D3/LuaSnip")
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-	use("rafamadriz/friendly-snippets")
+  -- snippets
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets")
 
-	-- managing & installing lsp servers, linters & formatters
-	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+  -- managing & installing lsp servers, linters & formatters
+  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
-	-- configuring lsp servers
-	use("neovim/nvim-lspconfig") -- easily configure language servers
-	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
 
-	-- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
-	use("amadeus/vim-mjml") -- mjml syntax support
+  -- formatting & linting
+  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+  use("amadeus/vim-mjml") -- mjml syntax support
+  -- PX to REM in styles
+  use("Oldenborg/vim-px-to-rem")
 
-	-- PX to REM in styles
-	use("Oldenborg/vim-px-to-rem")
+  -- treesitter configuration
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      require("nvim-treesitter.install").update({ with_sync = true })
+    end,
+  })
 
-	-- treesitter configuration
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			require("nvim-treesitter.install").update({ with_sync = true })
-		end,
-	})
+  use("windwp/nvim-autopairs")
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-	use("windwp/nvim-autopairs")
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+  -- git integration
+  use("tpope/vim-fugitive")
 
-	-- git integration
-	use("tpope/vim-fugitive")
+  -- Database
+  use("tpope/vim-dadbod")
+  use("kristijanhusak/vim-dadbod-ui")
 
-	-- Database
-	use("tpope/vim-dadbod")
-	use("kristijanhusak/vim-dadbod-ui")
-
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
