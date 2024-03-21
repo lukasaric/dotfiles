77ce0ced5b78c1fa2e376b6c9f178dff86fb36e6
Luka Saric <luka.saric@scayle.com>
2024-03-21 10:41:54 +0100

    fix lspconfig onattach

--- .config/nvim/after/plugin/lsp/lspconfig.lua
+++ .config/nvim/after/plugin/lsp/lspconfig.lua
@@ -1,41 +1,57 @@
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
+	return
 end
 
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-  return
+	return
 end
 
 local servers = require("lsaric.lsp.servers")
 
 local keymap = vim.keymap
 
--- enable keybinds only for when lsp server available
-local on_attach = function(_, bufnr)
-  -- keybind options
-  local opts = { noremap = true, silent = true, buffer = bufnr }
-  -- set keybinds
-  keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-  keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-  keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-  keymap.set("n", "<Leader>k", vim.lsp.buf.hover, opts)
-  keymap.set("n", "<Leader>K", vim.lsp.buf.signature_help, opts)
-  keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
-  keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
-  keymap.set("n", "gr", vim.lsp.buf.references, opts)
+local lsp_restart = function()
+  vim.diagnostic.reset()
+  vim.cmd("LspRestart")
+  print("LSP restarted")
 end
+
+vim.keymap.set('n', '<leader>lr', lsp_restart)
+vim.keymap.set('n', '<leader>m', '<cmd>Mason<cr>', { silent = true })
+vim.keymap.set('n', '<leader>N', vim.diagnostic.goto_prev)
+vim.keymap.set('n', '<leader>n', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>,', vim.diagnostic.setloclist)
+
+-- enable keybinds only for when lsp server available
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local buffer = ev.buf
+   	-- keybind options
+    local opts = { noremap = true, silent = true, buffer = buffer }
+    -- set keybinds
+    keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    keymap.set("n", "<Leader>k", vim.lsp.buf.hover, opts)
+    keymap.set("n", "<Leader>K", vim.lsp.buf.signature_help, opts)
+    keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, opts)
+    keymap.set({ "n", "v" }, "<Leader>ca", vim.lsp.buf.code_action, opts)
+    keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    end,
+})
 
 -- used to enable autocompletion (assign to every lsp server config)
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- setup lsp servers
 for _, server in ipairs(servers) do
-  local opts = { on_attach = on_attach, capabilities = capabilities }
-  local has_custom_opts, server_custom_opts = pcall(require, "lsaric.lsp.settings." .. server)
-  if has_custom_opts then
-    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-  end
-  lspconfig[server].setup(opts)
-end
+	local opts = {  capabilities = capabilities }
+	local has_custom_opts, server_custom_opts = pcall(require, "lsaric.lsp.settings." .. server)
+	if has_custom_opts then
+		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+	end
+	lspconfig[server].setup(opts)
+ end
--- .config/nvim/after/plugin/lsp/null-ls.lua
+++ .config/nvim/after/plugin/lsp/null-ls.lua
@@ -1,48 +1,58 @@
-local setup, null_ls = pcall(require, "null-ls")
-if not setup then
-  return
-end
-
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
--- to setup format on save
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local utils = require("null-ls.utils")
-local helpers = require("null-ls.helpers")
+local null_ls = require("null-ls")
 
 -- formatter settings: { <formatter name> : config }
-local cwdFn = helpers.cache.by_bufnr(function(params)
-  return utils.root_pattern("package.json")(params.bufname)
-end)
+local formatter_settings_map = {
+	prettier = {},
+}
+
+-- linter settings: { <linter name> : config }
+local linter_settings_map = {
+	eslint = {},
+}
+
+local get_source = function(type, name)
+	local none_ls_source = "none-ls." .. type .. "." .. name
+	local success_lsp_config, _ = pcall(require, none_ls_source)
+	if success_lsp_config then
+		return require(none_ls_source)
+	end
+	if null_ls.builtins[type] and null_ls.builtins[type][name] then
+		return null_ls.builtins[type][name]
+	end
+end
+
+-- null-ls setup
+local sources = {}
+for linter, config in pairs(linter_settings_map) do
+	local source = get_source("diagnostics", linter)
+	if source then
+		table.insert(sources, source.with(config))
+	end
+end
+-- formatters
+for formatter, config in pairs(formatter_settings_map) do
+	local source = get_source("formatting", formatter)
+	if source then
+		table.insert(sources, source.with(config))
+	end
+end
+
+-- format on save
+local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+local on_attach = function(client, bufnr)
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false })
+			end,
+		})
+	end
+end
 
 null_ls.setup({
-  -- setup formatters & linters
-  sources = {
-    --  to disable file types use
-    formatting.prettier,
-    formatting.stylua, -- lua formatter
-    formatting.stylelint.with({ cwd = cwdFn }), -- style formatter
-    diagnostics.stylelint.with({ cwd = cwdFn }),
-  },
-  -- configure format on save
-  on_attach = function(current_client, bufnr)
-    if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
-    end
-  end,
+	on_attach = on_attach,
+	sources = sources,
 })
