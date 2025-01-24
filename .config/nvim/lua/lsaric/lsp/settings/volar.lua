778384c14b3d2d7a1ccbe6636014cadd87342668
lukasaric <rpg.ari7@gmail.com>
2025-01-24 21:23:45 +0100

    fix volar settings

--- .config/nvim/lua/lsaric/lsp/settings/volar.lua
+++ .config/nvim/lua/lsaric/lsp/settings/volar.lua
@@ -3,5 +3,10 @@ return {
     vue = {
       hybridMode = false,
     },
+    typescript = {
+      tsdk = vim.fn.expand(
+        "~/.local/share/nvim/mason/packages/vue-language-server/node_modules/typescript/lib"
+      ),
+    }
   },
 }
